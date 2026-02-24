e0089f71158596e17ac5c25eb1b568c8df6c35f6
Nicole C. <nicoleinagan@gmail.com>
2026-02-24 10:29:00 -0500

    data_types

--- python/data_types.py
+++ python/data_types.py
@@ -1,1 +1,13 @@
+'''
+This is my first Python Script
+Developer: Nicole C. Inagan
+Description: This script let us understand how to is data types.
+'''
 print("Hello everyone")
+
+age = 17
+print(type(age))
+EARTH_GRAVITY = 9.8
+print(type(EARTH_GRAVITY))
+PI = 3.1416
+print(type(PI))
