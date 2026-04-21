438affe77bfe1709a725845a078d30ad8896f66d
Nicole.C <nichazantar.7125@unicesmag.edu.co>
2026-04-20 20:48:58 -0500

    basiccalc1

--- java/basic_calc1/src/main/java/com/mycompany/basic_calc1/Basic_calc1.java
+++ java/basic_calc1/src/main/java/com/mycompany/basic_calc1/Basic_calc1.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class Basic_calc1 {
 
     public static void main(String[] args) {
-        float num1=0, num2=0, add=0;
+        float num1=0, num2=0, add=0, subs=0, mult=0, div=0, avg=0;
         int opt=0;
         
         Scanner scanner =new Scanner(System.in);  
@@ -36,6 +36,36 @@ public class Basic_calc1 {
         if (opt==1){
             add= num1 + num2;
             System.out.println("Addition is: " + (num1 + num2));
+        }
+         if (opt==2){
+            subs = num1 - num2;
+            System.out.println("Substraction is: " + (num1 - num2));
+        }
+         if (opt==3){
+            mult = num1 * num2;
+            System.out.println("Multiplication is: " + (num1 * num2));
+        }
+         if (opt==4){
+            div = num1 / num2;
+            System.out.println("Division is: " + (num1 / num2));
+        }
+         if (opt==5){
+            avg = num1 + num2/2;
+            System.out.println("Average is: " + ((num1 + num2) /2));
+        }
+        if (opt==6){
+            add= num1 + num2;
+            System.out.println("Addition is: " + (num1 + num2));
+            subs = num1 - num2;
+            System.out.println("Substraction is: " + (num1 - num2));
+            mult = num1 * num2;
+            System.out.println("Multiplication is: " + (num1 * num2));
+            div = num1 / num2;
+            System.out.println("Division is: " + (num1 / num2));
+            avg= (num1 + num2) /2;
+            System.out.println("Average is: " + ((num1 + num2) /2));
         }else{
+            System.out.println("Error");
+        }
     }
 }
